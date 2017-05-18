b6a313dc8c20d1ecbf1e8592255ae0bfba80dece
Yevhen Shymko <imjust93@gmail.com>
2017-05-18 19:23:55 +0200

    Knapsack optimised

--- src/task_06_Knapsak/Main.java
+++ src/task_06_Knapsak/Main.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Created by Eugene on 11-May-17.
@@ -16,52 +17,72 @@ import java.util.Timer;
 public class Main {
     static List<Item> list = new ArrayList<>();
     static int limit = 0;
-    static int maxWeignt = 0;
     static int maxValue = 0;
     static String maxItem;
+    static boolean[] maxb;
+
 
     public static void main(String[] args) throws IOException {
         initData();
-        LocalDateTime timeStart = LocalDateTime.now(
-        );
+        LocalDateTime timeStart = LocalDateTime.now();
         int items = list.size();
         double pow = Math.pow(2, items);
         System.out.println(pow);
         for (int i = 0; i < pow; i++) {
-            if (i%100_000_000==0){
-                System.out.println(i);
+            int percent = i % 10_000_000;
+            if (percent ==0){
+                System.out.println(i/10_000_000);
             }
-            String binStr = intToBinaryShifting(i, items);
+//            String binStr = intToBinaryShifting(i, items);
+            boolean[] booleans = intToBinaryShiftingBoolean(i, items);
 //            System.out.println(binStr);
             int j = 0;
             int currentWeight=0;
             int currentValue=0;
-            for (char c : binStr.toCharArray()) {
-                if (c==1){
+//            for (char c : binStr.toCharArray()) {
+//                if (c==49){//askii num of 1
+//                    Item item = list.get(j);
+//                    currentValue += item.value;
+//                    currentWeight += item.weight;
+//                }
+//                j++;
+//            }
+            for (boolean b : booleans) {
+                if (b){//askii num of 1
                     Item item = list.get(j);
                     currentValue += item.value;
                     currentWeight += item.weight;
                 }
                 j++;
             }
-            if (currentWeight<limit) {
+            if (currentWeight>limit) {
                 continue;
             }
             if(maxValue<currentValue){
                 maxValue=currentValue;
-                maxItem=binStr;
+//                maxItem=binStr;
+                maxb = booleans;
+
             }
         }
-        System.out.println(maxItem);
-        LocalDateTime timeend = LocalDateTime.now(
-        );
-//        LocalDateTime minus = timeend.();
+        for (boolean b : maxb) {
+            if (b){
+                System.out.print("1");
+            }else
+                System.out.print("0");
+        }
+        System.out.println();
+
+        LocalDateTime timeEnd = LocalDateTime.now();
+        long seconds = ChronoUnit.SECONDS.between(timeStart, timeEnd);
+        System.out.println("Duration " + seconds);
+
 
 
     }
 
     private static void initData() throws IOException {
-        String pathToFolder = "C:\\Users\\Eugene\\IdeaProjects\\NAI\\src\\task_06_Knapsak\\data\\2";
+        String pathToFolder = "C:\\Users\\Eugene\\IdeaProjects\\NAI\\src\\task_06_Knapsak\\data\\15";
         readFile(Paths.get(pathToFolder), list);
     }
 
@@ -89,23 +110,6 @@ public class Main {
 
     }
 
-    public static String intToBinary(int n, int numOfBits) {
-        String binary = "";
-        for (int i = 0; i < numOfBits; ++i, n /= 2) {
-            int j = n % 2;
-            switch (j) {
-                case 0:
-                    binary = "0" + binary;
-                    break;
-                case 1:
-                    binary = "1" + binary;
-                    break;
-            }
-        }
-
-        return binary;
-    }
-
     public static String intToBinaryShifting(int n, int numOfBits) {
         int bitmask = 1;
         String binary = "";
@@ -117,5 +121,12 @@ public class Main {
         }
 //        }
         return binary;
+    }
+    private static boolean[] intToBinaryShiftingBoolean(int number, int base) {
+        final boolean[] ret = new boolean[base];
+        for (int i = 0; i < base; i++) {
+            ret[base - 1 - i] = (1 << i & number) != 0;
+        }
+        return ret;
     }
 }
